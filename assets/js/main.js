10541762b0a935d8fe25cd9663fb99546510ed21
emanuel-malungo <147757794+emanuel-malungo@users.noreply.github.com>
2023-12-14 12:50:22 +0100

    atualizacao 2.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,20 +16,6 @@ let btn_form = document.querySelector("#form-btn-cadastro");
 
 ScrollReveal().reveal('.punchline', { delay: 1000 });
 
-/**
- * scroll reveal effect
- */
-
-const sections = document.querySelectorAll("[data-section]");
-
-const scrollReveal = function () {
-  for (let i = 0; i < sections.length; i++) {
-    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
-      sections[i].classList.add("active");
-    }
-  }
-}
-
 scrollReveal();
 
 addEventOnElem(window, "scroll", scrollReveal);
